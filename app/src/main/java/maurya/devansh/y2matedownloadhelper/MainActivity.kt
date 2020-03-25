008169bbb622365b8acfd81a74d0e45630fe9a94
Devansh-Maurya <devansh233@gmail.com>
2020-03-25 18:54:51 +0530

    Getting a list of all the browser apps installed

--- app/src/main/java/maurya/devansh/y2matedownloadhelper/MainActivity.kt
+++ app/src/main/java/maurya/devansh/y2matedownloadhelper/MainActivity.kt
@@ -1,12 +1,28 @@
 package maurya.devansh.y2matedownloadhelper
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"))
+        val browsersList = packageManager.queryIntentActivities(browserIntent,
+            PackageManager.MATCH_ALL)
+
+        val listString = StringBuilder()
+        browsersList.forEach {
+            val packageName = it.activityInfo.packageName
+            listString.append("$packageName\n")
+        }
+
+        textView.text = listString
     }
 }
