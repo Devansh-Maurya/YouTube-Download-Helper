531dab482da56ceb4dc24b3daa26fee509565a62
Devansh-Maurya <devansh233@gmail.com>
2020-03-25 18:38:39 +0530

    Not showing download helper activity UI

--- app/src/main/java/maurya/devansh/y2matedownloadhelper/Y2MateHelperActivity.kt
+++ app/src/main/java/maurya/devansh/y2matedownloadhelper/Y2MateHelperActivity.kt
@@ -2,15 +2,13 @@ package maurya.devansh.y2matedownloadhelper
 
 import android.content.Intent
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_y2mate_helper.*
+import androidx.appcompat.app.AppCompatActivity
 
 class Y2MateHelperActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_y2mate_helper)
 
         when {
             intent?.action == Intent.ACTION_SEND -> {
@@ -20,6 +18,7 @@ class Y2MateHelperActivity : AppCompatActivity() {
                 }
             }
         }
+        finish()
     }
 
     private fun openUrlInBrowser(url: String) {
