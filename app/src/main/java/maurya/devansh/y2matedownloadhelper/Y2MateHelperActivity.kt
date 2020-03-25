fed6339b0be95293b3d2b2f663d568d6c5d4bdfc
Devansh-Maurya <devansh233@gmail.com>
2020-03-25 16:56:59 +0530

    Getting shared video URL in activity

--- app/src/main/java/maurya/devansh/y2matedownloadhelper/Y2MateHelperActivity.kt
+++ app/src/main/java/maurya/devansh/y2matedownloadhelper/Y2MateHelperActivity.kt
@@ -1,12 +1,29 @@
 package maurya.devansh.y2matedownloadhelper
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_y2mate_helper.*
 
 class Y2MateHelperActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_y2mate_helper)
+
+        when {
+            intent?.action == Intent.ACTION_SEND -> {
+                if (intent.type == "text/plain") {
+                    Log.d("Helper", intent.toString())
+                    textView.text = intent.getStringExtra(Intent.EXTRA_TEXT)
+                    openUrlInBrowser(intent.getStringExtra(Intent.EXTRA_TEXT) ?: "")
+                }
+            }
+        }
+    }
+
+    private fun openUrlInBrowser(url: String) {
+
     }
 }
