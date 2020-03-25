cbf6c2f058640275092c435a740fcaaa17bc393b
Devansh-Maurya <devansh233@gmail.com>
2020-03-25 21:35:06 +0530

    Setting browser list item color according to user preference

--- app/src/main/java/maurya/devansh/y2matedownloadhelper/BrowserItemViewHolder.kt
+++ app/src/main/java/maurya/devansh/y2matedownloadhelper/BrowserItemViewHolder.kt
@@ -1,8 +1,12 @@
 package maurya.devansh.y2matedownloadhelper
 
+import android.content.Context
 import android.content.pm.PackageManager
+import android.graphics.Color
 import android.graphics.drawable.Drawable
 import android.view.View
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.item_browser_list.view.*
 
@@ -14,6 +18,14 @@ class BrowserItemViewHolder(private val view: View, private val clickAction: (St
 
     fun bind(packageName: String) {
         view.apply {
+
+            if (packageName == getDefaultBrowserPackageName()) {
+                val colorPrimary = ContextCompat.getColor(view.context, R.color.colorPrimary)
+                setCardAppearance(colorPrimary, Color.WHITE)
+            } else {
+                setCardAppearance(Color.WHITE, Color.BLACK)
+            }
+
             browserIconView.setImageDrawable(getBrowserIcon(packageName))
 
             val browserName = getBrowserName(packageName)
@@ -31,5 +43,18 @@ class BrowserItemViewHolder(private val view: View, private val clickAction: (St
         val pm = view.context.packageManager
         val applicationInfo = pm.getApplicationInfo(packageName, PackageManager.GET_META_DATA)
         return pm.getApplicationLabel(applicationInfo)
+    }
+
+    private fun getDefaultBrowserPackageName(): String {
+        val prefs = view.context.getSharedPreferences(BrowserChooserActivity.BROWSER_PREF, Context.MODE_PRIVATE)
+        return prefs.getString(BrowserChooserActivity.DEFAULT_BROWSER,
+            "com.android.chrome") ?: "com.android.chrome"
+    }
+
+    private fun setCardAppearance(backgroundColor: Int, textColor: Int) {
+        (view as CardView).apply {
+            setCardBackgroundColor(backgroundColor)
+            browserNameTV.setTextColor(textColor)
+        }
     }
 }
