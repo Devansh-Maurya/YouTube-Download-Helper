c86d1bf23f53b19329bdd467c5782168bbfbf757
Devansh-Maurya <devansh233@gmail.com>
2020-03-25 16:43:41 +0530

    Added an activity to accept share intent

---
+++ app/src/main/java/maurya/devansh/y2matedownloadhelper/Y2MateHelperActivity.kt
@@ -0,0 +1,12 @@
+package maurya.devansh.y2matedownloadhelper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Y2MateHelperActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_y2mate_helper)
+    }
+}
